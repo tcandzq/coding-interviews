76c8eeb7ff429f5c5461ae6e245b4cfb855f23b7
tcandzq <2448424636@qq.com>
2019-11-30 22:13:51 +0800

    补充43从1到n整数中1出现的次数—_数位dp解法+cpp+20191130

--- 43_NumberOf1/NumberOf1.cpp
+++ 43_NumberOf1/NumberOf1.cpp
@@ -7,10 +7,68 @@
 
 */
 
+#include<vector>
+#include <iostream>
+
+using namespace std;
 
 class Solution {
 public:
+    vector<int> digit;
+    vector<vector<int>> memo;
     int countDigitOne(int n) {
+        return solve(n);
+    }
+
+    int solve(int n)
+    {
+        int len = 0;
+        digit.assign(64,0);
+        while (n)
+        {
+            digit[++len] = n % 10;
+            n /= 10;
+        }
+        cout << len <<endl;
+        memo.assign(len,vector<int>(len+1,-1));
+    
+        return dfs(len,0,1,1);
+    }
+
+    int dfs(int pos,int sm,bool lead /*前导零*/,bool limit /*数位上界变量*/)
+    {
+        if (pos == 0)
+        {
+           return sm;
+        }
+        if (!lead && !limit && memo[pos][sm] != -1)
+        {
+            return memo[pos][sm];
+        }
+        int up = limit ? digit[pos] : 9; //根据limit判断枚举的上界up;
+        int ret = 0;
+        //开始计数
+        for (int i = 0; i <= up; ++i) //枚举，然后把不同情况的个数加到ret就可以了
+        {
+            ret += dfs(pos - 1,/*状态转移*/ sm + (i == 1),lead && (i==0),limit && (i == up));
+            // 大概就是说，我当前数位枚举的数是i，然后根据题目的约束条件分类讨论去计算不同情况下的个数，还有要根据state变量来保证i的合法性，
+        }
+        if (! limit && !lead) //计算完，记录状态
+        {
+           memo[pos][sm] = ret;
+        }
+        return ret;
         
     }
 };
+
+
+int main(int argc, char const *argv[])
+{
+    int n  = 20;
+
+    Solution  solution;
+
+    cout << solution.countDigitOne(n);
+    return 0;
+}
